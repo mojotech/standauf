0e68ba2d514aa9959889994b44ec3bdddceeabd1
Sam Saccone <samccone@gmail.com>
2015-08-18 18:28:21 -0400

    fix jshint warnings

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,3 @@
-(function(document) {
+(function() {
   'use strict';
-
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-
 })(document);
